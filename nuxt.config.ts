34420a6bdca3f45e969a604dcf0a14b79f54f48d
Andy Willekens <andywillekens@gmail.com>
2023-12-18 21:35:14 +0100

    Added nuxt studio

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { resolve } from 'path'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxtjs/tailwindcss'],
+  modules: ['@nuxt/content', '@nuxtjs/tailwindcss', '@nuxthq/studio'],
   alias: {
     '@': resolve(__dirname, './src')
   },
