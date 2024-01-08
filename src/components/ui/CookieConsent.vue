85b0749ff258d6722fb8aecc8b79ac8cc9cd3672
Andy Willekens <andywillekens@gmail.com>
2024-01-08 09:22:23 +0100

    Fixed animation for cookie consent

--- src/components/ui/CookieConsent.vue
+++ src/components/ui/CookieConsent.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
   const { gtag, grantConsent, revokeConsent } = useGtag()
   const isTracking = ref(false)
+  const animationPlayed = useCookie('anim')
+
+  setTimeout(() => {
+    animationPlayed.value = 'played'
+  }, 1000)
 
   const grand = () => {
     grantConsent()
@@ -14,8 +19,9 @@
 <template>
   <div
     v-if="!isTracking"
+    :class="!!animationPlayed ? '' : '-translate-x-[calc(100%_+_4rem)] animate-slide-in-from-left'"
     id="cookieConsent"
-    class="flex flex-col fixed left-4 sm:left-8 bottom-4 sm:bottom-8 right-4 z-50 bg-white self-start rounded-2xl shadow-xl shadow-mirage-900/20 border border-mirage-900/10 gap-4 max-w-80 -translate-x-[calc(100%_+_4rem)] animate-slide-in-from-left">
+    class="flex flex-col fixed left-4 sm:left-8 bottom-4 sm:bottom-8 right-4 z-50 bg-white self-start rounded-2xl shadow-xl shadow-mirage-900/20 border border-mirage-900/10 gap-4 max-w-80">
     <section class="flex flex-col p-6 pb-0 gap-4">
       <span class="flex gap-2 font-head tracking-tight font-bold text-2xl text-mirage-900">
         Koekje?<Icon name="ph:cookie-duotone" class="text-purple-600" size="28" />
