aada2a735194c5525aed7a1b5f63e1d595722b02
Andy Willekens <andywillekens@gmail.com>
2024-11-18 13:53:15 +0100

    Update AppNavigation.vue
    Fixed urls

--- src/components/ui/navigation/AppNavigation.vue
+++ src/components/ui/navigation/AppNavigation.vue
@@ -3,13 +3,13 @@
     class="appNav flex flex-col md:flex-row md:items-center gap-4 md:border-l md:border-purple-600/10 md:pl-4 lg:pl-8">
     <CoreButton
       class="!text-base"
-      to="https://app.popsup.nl/login"
+      to="https://app.popsup.andywillekens.nl/login"
       label="Inloggen"
       tag="nuxt-link"
       theme="alt-grey" />
     <CoreButton
       class="!text-base"
-      to="https://app.popsup.nl/create-account"
+      to="https://app.popsup.andywillekens.nl/create-account"
       label="Start proefperiode"
       tag="nuxt-link"
       theme="cta-alt" />
